44d03404e3992b237e5ff5662fea2a3d2a7c8705
TYXuan <yxtan92@hotmail.com>
2023-06-09 00:10:49 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cat ~/access_token.txt | docker login --username tyxuan --password-stdin
-docker run -d --name e9c79ca11ef4e7e4b8bc8ae078f0c89d56e75c45007b1bef3eff1418ff045376 -it f9c14fe76d50
-docker exec e9c79ca11ef4e7e4b8bc8ae078f0c89d56e75c45007b1bef3eff1418ff045376 pwd /usr/share
+docker login
+#cat ~/access_token.txt | docker login --username tyxuan --password-stdin
+#docker run -d --name e9c79ca11ef4e7e4b8bc8ae078f0c89d56e75c45007b1bef3eff1418ff045376 -it f9c14fe76d50
+#docker exec e9c79ca11ef4e7e4b8bc8ae078f0c89d56e75c45007b1bef3eff1418ff045376 pwd /usr/share
 docker cp e9c79ca11ef4e7e4b8bc8ae078f0c89d56e75c45007b1bef3eff1418ff045376:/usr/share/COVID-19_Radiography_Dataset data/
 python src/dataprocessing.py
 python src/modeling.py
