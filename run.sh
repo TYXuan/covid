e55b0774bf8ceae9fa106ff6e2fe3ad95735e3ee
TYXuan <yxtan92@hotmail.com>
2023-06-09 15:42:21 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker login
+docker pull tyxuan/covid:tagname
 #cat ~/access_token.txt | docker login --username tyxuan --password-stdin
 docker run -d --name 86a85c70adef -it f9c14fe76d50
 docker exec 86a85c70adef pwd /usr/share
